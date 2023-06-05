b15bef77e71b5ba1f4df3f4c2d12e017c3d3f88b
Luca Saccarola <github.e41mv@aleeas.com>
2023-06-05 15:44:43 +0200

    feat: don't run if editorconfig is detected

--- lua/tidy/init.lua
+++ lua/tidy/init.lua
@@ -64,7 +64,7 @@ function M.setup(opts)
   vim.api.nvim_create_autocmd("BufWritePre", {
     group = tidy_grp,
     callback = function()
-      if not M.enabled or vim.b.tidy_enabled == false or is_excluded_ft(opts) then
+      if not M.enabled or vim.b.tidy_enabled == false or is_excluded_ft(opts) or not vim.tbl_isempty(vim.b.editorconfig) then
         return false
       end
 
