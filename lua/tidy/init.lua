842cd4c7cdd95c6d76ed64f7e7b621781eb475c2
McAuley Penney <jacobmpenney@gmail.com>
2024-12-30 16:31:49 -0700

    refactor: trim lines modified in insert mode, not in normal
    In normal mode, actions like undo, redo, and deleting lines will affect
    what is trimmed. This leads to trims on lines that were not really
    touched by the user, giving errant trimmings.
    
    Trim only the lines touched in insert mode. This means that lines that
    the user edited in normal will not be trimmed (not ideal), but there
    should be no errant trimmings (better than errors).

--- lua/tidy/init.lua
+++ lua/tidy/init.lua
@@ -4,10 +4,15 @@ local M = {}
 M.opts = {
   enabled_on_save = true,
   filetype_exclude = {},
-  only_modified_lines = false,
+  only_insert_lines = false,
   provide_undefined_editorconfig_behavior = false,
 }
 
+local function is_valid_bt()
+  return v.bo.buftype ~= "terminal" and
+      v.bo.buftype ~= "nofile" and
+      v.bo.buftype ~= "prompt"
+end
 
 local function is_excluded_ft(excluded_fts)
   local ft = v.api.nvim_buf_get_option(0, "filetype")
@@ -23,33 +28,41 @@ local function reset_cursor_pos(pos)
   v.api.nvim_win_set_cursor(0, pos)
 end
 
-local function remove_trailing_ws_in_range(lines, start_idx, end_idx)
-  for i = start_idx, end_idx do
-    lines[i] = lines[i]:gsub("%s+$", "")
-  end
-end
-
 local function trim_modified_lines()
-  if not v.b.tidy_saved_lines then
+  if not v.b.tidy_mod_lines then
     return
   end
 
   local new_lines = v.api.nvim_buf_get_lines(0, 0, -1, false)
 
-  local old_text = table.concat(v.b.tidy_saved_lines, "\n")
-  local new_text = table.concat(new_lines, "\n")
-
-  local hunks = v.diff(old_text, new_text, { result_type = "indices" })
-
-  for _, hunk in ipairs(hunks) do
-    -- hunk = { start_old, len_old, start_new, len_new }
-    local start_new, len_new = hunk[3], hunk[4]
-    if len_new > 0 then
-      remove_trailing_ws_in_range(new_lines, start_new, start_new + len_new - 1)
+  for i, val in pairs(v.b.tidy_mod_lines) do
+    if val == true and new_lines[i] then
+      new_lines[i] = new_lines[i]:gsub("%s+$", "")
     end
   end
 
   v.api.nvim_buf_set_lines(0, 0, -1, false, new_lines)
+end
+
+local function track_insert_changes()
+  local cur_lines = v.api.nvim_buf_get_lines(0, 0, -1, false)
+
+  local old_text = table.concat(v.b.tidy_start_lines, "\n")
+  local new_text = table.concat(cur_lines, "\n")
+  local hunks = v.diff(old_text, new_text, { result_type = "indices" })
+
+  local changes = v.b.tidy_mod_lines
+
+  for _, hunk in ipairs(hunks) do
+    -- hunk = { start_old, len_old, start_new, len_new }
+    local start_new, len_new = hunk[3], hunk[4]
+    for i = start_new, start_new + len_new - 1 do
+      changes[i] = true
+    end
+  end
+
+  v.b.tidy_mod_lines = changes
+  v.b.tidy_start_lines = cur_lines
 end
 
 function M.toggle()
@@ -62,15 +75,13 @@ function M.toggle()
   end
 end
 
-function M.run(trim_ws)
+function M.run()
   local cursor_pos = v.api.nvim_win_get_cursor(0)
 
-  if trim_ws then
-    if M.opts.only_modified_lines then
-      trim_modified_lines()
-    else
-      v.cmd([[:keepjumps keeppatterns %s/\s\+$//e]])
-    end
+  if M.opts.only_insert_lines then
+    trim_modified_lines()
+  else
+    v.cmd([[:keepjumps keeppatterns %s/\s\+$//e]])
   end
 
   v.cmd([[:keepjumps keeppatterns silent! 0;/^\%(\n*.\)\@!/,$d_]])
@@ -86,13 +97,28 @@ function M.setup(opts)
   v.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
     group = tidy_grp,
     callback = function()
-      v.b.tidy_saved_lines = v.api.nvim_buf_get_lines(0, 0, -1, false)
+      if not is_valid_bt() then return end
+
+      v.b.tidy_start_lines = v.api.nvim_buf_get_lines(0, 0, -1, false)
+      v.b.tidy_mod_lines = {}
+    end
+  })
+
+  v.api.nvim_create_autocmd("TextChangedI", {
+    group = tidy_grp,
+    callback = function()
+      if not is_valid_bt() then return end
+
+
+      track_insert_changes()
     end
   })
 
   v.api.nvim_create_autocmd("BufWritePre", {
     group = tidy_grp,
     callback = function()
+      if not is_valid_bt() then return end
+
       if not M.opts.enabled_on_save or is_excluded_ft(M.opts.filetype_exclude) then
         return
       end
@@ -108,14 +134,19 @@ function M.setup(opts)
         end
       end
 
-      M.run(trim_ws)
+      if trim_ws then
+        M.run()
+      end
     end
   })
 
   v.api.nvim_create_autocmd("BufWritePost", {
     group = tidy_grp,
     callback = function()
-      v.b.tidy_saved_lines = v.api.nvim_buf_get_lines(0, 0, -1, false)
+      if not is_valid_bt() then return end
+
+      v.b.tidy_start_lines = v.api.nvim_buf_get_lines(0, 0, -1, false)
+      v.b.tidy_mod_lines = {}
     end
   })
 end
