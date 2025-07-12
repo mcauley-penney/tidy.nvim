bf836359f0e7bceb60c93f8f9cc40e0e1f13d205
McAuley Penney <jacobmpenney@gmail.com>
2025-07-12 15:13:29 -0700

    refactor(deprecated): nvim_buf_get_option() → nvim_get_option_value()

--- lua/tidy/init.lua
+++ lua/tidy/init.lua
@@ -15,7 +15,7 @@ local function is_valid_bt()
 end
 
 local function is_excluded_ft(excluded_fts)
-  local ft = v.api.nvim_buf_get_option(0, "filetype")
+  local ft = v.api.nvim_get_option_value("filetype", {})
   local contains = v.fn.has("nvim-0.10") == 1 and v.list_contains
     or v.tbl_contains
   return contains(excluded_fts, ft)
@@ -98,7 +98,7 @@ function M.setup(opts)
     callback = function()
       if not is_valid_bt() then return end
 
-      v.b.tidy_start_lines = v.api.nvim_buf_get_lines(0, 0, -1, false)
+      v.b.tidy_start_lines = v.api.nvim_buf_get_lines(0, 0, -1, false) or { "" }
       v.b.tidy_mod_lines = {}
     end,
   })
